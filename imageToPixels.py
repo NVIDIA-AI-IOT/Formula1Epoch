6a305f96d934d746c6e3f213d61ae86448bfe314
Ricky Avina <rickyavina@icloud.com>
2017-06-27 12:05:32 -0700

    Added directory function

--- imageToPixels.py
+++ imageToPixels.py
@@ -1,5 +1,20 @@
 from PIL import Image
 from numpy import*
+from os import listdir
+
+def loadImagesAsPixels(path):
+    # return array of images
+    imagesList = listdir(path)
+    loadedImages = []
+    pixelList = []
+    for image in imagesList:
+        img = Image.open(path + image)
+        pixelList.append(imageToPixels(image))
+        loadedImages.append(img)
+
+    print(loadedImages)
+    print(pixelList)
+    return pixelList
 
 def imageToPixels(image):
     temp=asarray(Image.open(image))
