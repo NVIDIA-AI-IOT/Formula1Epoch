193c185e59c771fc29216a7bef58d43f5dbfda04
Abhi12-Ayalur <abhinav.ayalur@gmail.com>
2017-07-03 12:06:37 -0700

    commented NN code

--- SteerNet.py
+++ SteerNet.py
@@ -10,56 +10,62 @@ import helperFunctions
 from keras.utils import plot_model
 
 def model():
-    # model with 3 hidden layers
+    #Model with 3 hidden layers
+    #Input takes in image
     img = Input(shape = (672, 376, 3), name = 'img')
-
+    #Convolution/Pooling Layer 1
     x = Convolution2D(8, 3, 3)(img)
     x = Activation('relu')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-
+    #Convolution/Pooling Layer 2
     x = Convolution2D(16, 3, 3)(x)
     x = Activation('relu')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-
+    #Convolution/Pooling Layer 3
     x = Convolution2D(32, 3, 3)(x)
     x = Activation('relu')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-
+    #Flattens into 1D array for Usage in final layer
     merged = Flatten()(x)
-
+    #One final fully connected layer for figuring output values
     x = Dense(128)(merged)
     x = Activation('linear')(x)
     x = Dropout(.3)(x)
-
+    #Final output
     jstk = Dense(1, name='jstk')(x)
-
+    #Compiled and initializes the model
     steerNet = Model(input=[img], output=[jstk])
     steerNet.compile(optimizer='adam', loss='mean_squared_error')
     print(steerNet.summary())
     return steerNet
 
 def trainModel(model, imgIn, jstkOut):
-    #trains predefined model with verbose logging
+    #Trains predefined model with verbose logging, input image data and output steering data
     model.fit(x=imgIn, y=jstkOut, batch_size=32, epochs=100, verbose=2, callbacks=None, validation_split=0.2, shuffle=True, initial_epoch=0)
     modelName = raw_input("Please enter the trained models filename")
     modelPng = modelName + ".png"
     modelName = modelName + ".h5"
+    #Plots the trained model
     plot_model(steerNet, to_file=modelPng)
     model.save(modelName)
     print("Saved as %s" %(modelName) )
     return model
 
 def testModel(model, testX, testY):
-    # Test model and evauluate accuracy
+    # Test model and evauluate accuracy, prints it
     scores = model.evaluate(testX, testY)
     print("\nAccuracy: " + model.metrics_name[1], scores[1]*100)
 
 def main():
+    #Main Function, starts with path inputs
     imagePath = raw_input("Please enter the filepath to your images folder")
     labelPath = raw_input("Please enter the filepath to your labels folder")
+    #Uses helper functions to get array of images and outputs
     imgAr, testX = helperFunctions.getTrainingData(imagePath)
     jstkAr, testY = helperFunctions.parseTextFile(labelPath)
+    #Runs model function to initialize model
     steerModel = model()
+    #Trains model with the function
     trModel = trainModel(steerModel, imgAr, jstkAr)
     testModel(model, testX, testY)
 
--- helperFunctions.py
+++ helperFunctions.py
@@ -3,9 +3,10 @@ import numpy as np
 import glob
 import os
 
-# All standalone helper functions can be defined here
+#All standalone helper functions can be defined here
 
 def getTrainingData(p):
+    #Parses the filepath to access files
     if p.startswith("/"):
         if p.endswith("/"):
             path = p
@@ -17,32 +18,32 @@ def getTrainingData(p):
         else:
             path = os.getcwd() + "/" + p + "/"
 
-    # return training sata
+    #Return training data
     pixelList = []
 
+    #Opens all images in the given filepath
     for filename in glob.glob(path+"*.jpg"):
         im = Image.open(filename)
         #print(im)
         pixelList.append(imageToPixels(im))
-
+    #Splits images into validation and training date
     trainX, finalX = splitImage(pixelList)
     return trainX, finalX
 
 def imageToPixels(image):
+    #Resizes the image and extracts pixels
     resize = image.resize((672, 376), Image.NEAREST)
-    #temp = np.array(resize)
     image_convert = np.swapaxes(np.swapaxes(resize, 1, 2), 0, 1)
     img = np.array(image_convert)
-    #print(img)
     return img
 
 def parseTextFile(path):
-    # works forheight csv or txt files
+    #Opens text file with given steering values in a '<>,<>,<>' format
     f = open(path, 'r')
 
     readings = []
     data = []
-
+    #Formats and extracts data, appending to array
     for t in f:
         readings.append(t.strip('\n'))
 
@@ -50,19 +51,20 @@ def parseTextFile(path):
 
         arr = r.split(',')
         data.append(arr)
-
+    #Splits text data for steering into training and validation
     trainY, finalY = splitList(data)
     return trainY, finalY
 
 def splitImage(array):
-    # 4/5 of data is training data, the rest is testing data
+    #4/5 of data is training data, the rest is testing data
     split = len(array)*4/5
     normalArray = array[:split]
     testArray = array[split:]
+    #Returns split
     return normalArray, testArray
 
 def splitList(bigAr):
-    # 4/5 of data is training data, the rest is testing data
+    #4/5 of data is training data, the rest is testing data
     arrayG = bigAr[0]
     array = []
 
@@ -72,6 +74,7 @@ def splitList(bigAr):
     split = len(array)*4/5
     normalArray = array[:split]
     testArray = array[split:]
+    #Returns split
     return normalArray, testArray
 
-g,x = getTrainingData("testDir")
+#g,x = getTrainingData("testDir")
