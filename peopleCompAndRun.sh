fd86a27601b31b402a086bf56f19f1f1a8a1af17
abhi12-ayalur <abhinav.ayalur@gmail.com>
2017-07-12 15:07:37 -0700

    removed while statement

---
+++ peopleCompAndRun.sh
@@ -0,0 +1,5 @@
+cd /home/nvidia/Formula1Epoch/jetson-inference/build/
+cmake ../
+make
+cd aarch64/bin/
+./detectnet-console
---
+++ runPeopleNet.sh
@@ -0,0 +1,2 @@
+cd /home/nvidia/Formula1Epoch/jetson-inference/build/aarch64/bin/
+./detectnet-console
