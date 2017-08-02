31ed531c0caff7651e4cbdf0990189ea744b626e
nitroxplunge <isaac@wilcove.com>
2017-08-02 09:44:40 -0700

    Cleaned up main directory

--- datacollection/data/lidar/infer_lidar.py
+++ datacollection/data/lidar/infer_lidar.py
@@ -1,15 +1,30 @@
 from rplidar import RPLidar
 import time
 import numpy as np
+import sys
+import signal
 
-lidar = RPLidar('/dev/ttyUSB0')
+def ctrlc_exit(signal, frame):
+	lidar.stop()
+	lidar.stop_motor()
+	lidar.disconnect()
+	print("\nLiDAR stopped")
+	sys.exit(0)
 
-for scan in lidar.iter_scans():
-	np.save("/home/nvidia/lidar.npy", scan)
-	print("saved")
-	#print('saved: ' + str(np.load('/home/nvidia/lidar.npy')))
+signal.signal(signal.SIGINT, ctrlc_exit)
 
-lidar.stop()
-lidar.stop_motor()
-lidar.disconnect()
-#
+for i in range (100):
+
+	try:
+		lidar = RPLidar('/dev/ttyUSB0')
+
+		print("Reading LiDAR...")
+
+		for scan in lidar.iter_scans():
+			np.save("/home/nvidia/lidar.npy", scan)
+
+		lidar.stop()
+		lidar.stop_motor()
+		lidar.disconnect()
+	except:
+		pass
